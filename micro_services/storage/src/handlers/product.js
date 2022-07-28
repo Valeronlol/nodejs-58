7cbfb08901228f3961aa7cd50495e89aa536b855
Valerii Kuzivanov <valerii.kuzivanov@gmail.com>
2022-07-28 16:05:56 +0300

    depends up

--- micro_services/storage/src/handlers/product.js
+++ micro_services/storage/src/handlers/product.js
@@ -1,8 +1,6 @@
 const dbClient = require('../db-client')
 
 exports.getList = async (ctx) => {
-    console.log('CONSOLE: ctx', ctx.headers)
-    throw new Error('Something happened! :(')
     ctx.body = await dbClient.product.findMany({
         take: 50
     })
